1bb8d42d69c2f9c149abcc5e1807ea5b4b1bf699
MacBookPro <macbookpro@MacBook-Pro-de-MacBookPro.local>
2025-04-01 17:38:16 -0300

    terminando el carrousel

--- src/components/carrousel/Carrousel.jsx
+++ src/components/carrousel/Carrousel.jsx
@@ -1,50 +1,168 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./carrousel.css";
+import corpo from "../../../public/images/corpo.jpg";
+import boda from "../../../public/images/boda.jpg";
+import artistico from "../../../public/images/artistico.jpg";
+import convenciones from "../../../public/images/convenciones.jpg";
+import publico from "../../../public/images/publico.jpg";
 
-const Carrousel = () => {
-  const [items, setItems] = useState([
-    { id: 1, text: "Caja 1" },
-    { id: 2, text: "Caja 2" },
-    { id: 3, text: "Caja 3" },
-  ]);
+const Carrousel = ({ event }) => {
+  const [items, setItems] = useState([]);
+  console.log(items);
+  const [offsetY, setOffsetY] = useState(0);
+  const getVH = (vh) => (window.innerHeight * vh) / 100;
+  const refs = useRef([]);
 
-  const [caja, setCaja] = useState([0, 0]);
+  const originalItems = [
+    {
+      id: 1,
+      alt: "corpo",
+      img: corpo,
+      title: "Eventos Corporativos",
+      text: "",
+      text2: "",
+    },
+    {
+      id: 2,
+      alt: "artistico",
+      img: artistico,
+      title: "Espectaculos Artisticos",
+      text: "",
+      text2: "",
+    },
+    {
+      id: 3,
+      alt: "boda",
+      img: boda,
+      title: "Eventos Sociales",
+      text: "En bodas, cumpleaños, fiestas deaniversario y otras celebraciones familiares, la ambientación juega un papel clave para crear momentos memorables. La iluminación decorativa se adapta a la visión de los organizadores, creando unaatmósfera cálida y personalizada que reliza el estilo del evento",
 
-  const handleRotate = (n) => {
-    const newCaja = items.map((item) => item.id).filter((id) => id !== n);
-    setCaja(newCaja);
+      text2:
+        "El sonido debe estar cuidadosamenteequilibrado, para que la música, losdiscursos y otros momentos especiales sedisfruten sin interrupciones y con laclaridad adecuada. Las pantallas puedenusarse para proyectar fotos, videos opresentaciones que personalicen aún másel evento, aportando un toque emotivo yúnico a cada ocasión.",
+    },
+    {
+      id: 4,
+      alt: "convenciones",
+      img: convenciones,
+      title: "Convenciones y Exposiciones",
+      text: "",
+      text2: "",
+    },
+    {
+      id: 5,
+      alt: "publico",
+      img: publico,
+      title: "Espectaculos Publicos",
+      text: "",
+      text2: "",
+    },
+  ];
 
-    handleRotate2();
+  useEffect(() => {
+    const handleOrder = () => {
+      const selectedEvent = Number(event);
+      const selected = originalItems.findIndex(
+        (item) => item.id === selectedEvent
+      );
+
+      if (selectedEvent !== -1) {
+        const n = originalItems.length;
+        const center = Math.floor(n / 2);
+        const shift = (selected - center + n) % n;
+
+        const reorderedItems = [
+          ...originalItems.slice(shift),
+          ...originalItems.slice(0, shift),
+        ];
+
+        setItems(reorderedItems);
+        console.log(reorderedItems);
+      }
+    };
+
+    handleOrder();
+  }, [event]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          // Verificamos si el item en index 3 está visible
+          if (entry.isIntersecting && entry.target.dataset.index === "3") {
+            setTimeout(() => {
+              setItems((prevItems) => {
+                return [...prevItems.slice(1), prevItems[0]];
+              });
+            }, 3000); // Esperar 3 segundos
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    // Observar cada item
+    refs.current.forEach((el) => {
+      if (el) observer.observe(el);
+    });
+
+    return () => {
+      refs.current.forEach((el) => {
+        if (el) observer.unobserve(el);
+      });
+    };
+  }, [items]);
+
+  const moveUp = () => {
+    setOffsetY((prev) => prev - getVH(90) - 20);
   };
 
-  const handleRotate2 = () => {
-    setTimeout(() => {
-      setItems((prev) => {
-        const newItems = [...prev];
-        newItems.push(newItems.shift());
-        setCaja([0, 0]);
-        return newItems;
-      });
-    }, 500);
-
-    return;
+  const moveDown = () => {
+    setOffsetY((prev) => prev + getVH(90) + 20);
   };
 
   return (
     <div className="carouselContainer">
-      <div className="carouselTrack">
+      <div
+        className="carouselTrack"
+        style={{
+          transform: `translateY(${offsetY}px)`,
+          transition: "transform 0.5s ease-in-out",
+        }}
+      >
         {items.map((item, index) => (
           <div
-            key={item.id}
-            className={`carouselItem ${caja.includes(item.id) ? "moveUp" : ""}`}
+            ref={(el) => (refs.current[index] = el)}
+            style={{ backgroundColor: item.color }}
+            key={index}
+            className="carouselItem"
           >
-            <button
-              onClick={() => handleRotate(item.id)}
-              className="carouselButton"
-            >
-              Cerrar (Mover Arriba)
-            </button>
-            {item.text}
+            <div className={`eventImage ${item.alt}`}></div>
+
+            <div className="eventContent">
+              <div className="closeContainer">
+                <a href="/" className="carouselButton">
+                  x
+                </a>
+                <button onClick={() => moveUp()}>A</button>
+                <button onClick={() => moveDown()}>V</button>
+              </div>
+
+              <div className="eventText">
+                <h2 className="eventTitle">{item.title}</h2>
+                <div className="eventDescription">
+                  <p>{item.text}</p>
+                  <p>{item.text2}</p>
+                </div>
+              </div>
+
+              <div className="contactContainer">
+                <div className="contactText">
+                  <span>Listo para el espectaculo</span>
+                  <h3>Contactate con nosotros</h3>
+                </div>
+                <button>enviar mensaje</button>
+              </div>
+            </div>
           </div>
         ))}
       </div>
